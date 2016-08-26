6d12aba95859a9de660e78694e66b0d0896c3608
Michael <xbeastguyx@gmail.com>
2016-08-26 07:44:36 -0400

    Update gmod_currency.lua

--- gmod_currency.lua
+++ gmod_currency.lua
@@ -1,48 +1,48 @@
 GCurrency = GCurrency or {}
 
 function GCurrency:Add( player, type, amount )
-  local currencyTypes = {
-    [ "darkrp" ] = function( player, amount ) player:addMoney( amount ) end,
-    [ "pointshop1" ] = function( player, amount ) player:PS_GivePoints( amount ) end,
-    [ "pointshop2_standard" ] = function( player, amount ) player:PS2_AddStandardPoints( amount ) end,
-    [ "pointshop2_premium" ] = function( player, amount ) player:PS2_AddPremiumPoints( amount ) end
-  }
+	local currencyTypes = {
+		[ "darkrp" ] = function( player, amount ) player:addMoney( amount ) end,
+		[ "pointshop1" ] = function( player, amount ) player:PS_GivePoints( amount ) end,
+		[ "pointshop2_standard" ] = function( player, amount ) player:PS2_AddStandardPoints( amount ) end,
+		[ "pointshop2_premium" ] = function( player, amount ) player:PS2_AddPremiumPoints( amount ) end
+	}
   
-  if !currencyTypes[ type:lower() ] then
-    error "Invalid currency specified!"
-  end
+	if !currencyTypes[ type:lower() ] then
+		error "Invalid currency specified!"
+	end
   
-  currencyTypes[ type:lower() ]( player, amount )
+	currencyTypes[ type:lower() ]( player, amount )
 end
 
 function GCurrency:Take( player, type, amount )
-  local currencyTypes = {
-    [ "darkrp" ] = function( player, amount ) player:addMoney( -amount ) end,
-    [ "pointshop1" ] = function( player, amount ) player:PS_TakePoints( amount ) end,
-    [ "pointshop2_standard" ] = function( player, amount ) player:PS2_AddStandardPoints( -amount ) end,
-    [ "pointshop2_premium" ] = function( player, amount ) player:PS2_AddPremiumPoints( -amount ) end
-  }
+	local currencyTypes = {
+		[ "darkrp" ] = function( player, amount ) player:addMoney( -amount ) end,
+		[ "pointshop1" ] = function( player, amount ) player:PS_TakePoints( amount ) end,
+		[ "pointshop2_standard" ] = function( player, amount ) player:PS2_AddStandardPoints( -amount ) end,
+		[ "pointshop2_premium" ] = function( player, amount ) player:PS2_AddPremiumPoints( -amount ) end
+	}
   
-  if !currencyTypes[ type:lower() ] then
-    error "Invalid currency set!"
-  end
+	if !currencyTypes[ type:lower() ] then
+		error "Invalid currency set!"
+	end
   
-  currencyTypes[ type:lower() ]( player, amount )
+	currencyTypes[ type:lower() ]( player, amount )
 end
 
 function GCurrency:Get( player, type )
-  local amount
+	local amount
 
-  local currencyTypes = {
-    [ "darkrp" ] = function( player ) amount = player:getDarkRPVar( "money" ) end,
-    [ "pointshop1" ] = function( player ) amount = player:PS_GetPoints() end,
-    [ "pointshop2_standard" ] = function( player ) if player.PS2_Wallet then amount = player.PS2_Wallet.points end end,
-    [ "pointshop2_premium" ] = function( player ) if player.PS2_Wallet then amount = player.PS2_Wallet.premiumPoints end end
-  }
+	local currencyTypes = {
+		[ "darkrp" ] = function( player ) amount = player:getDarkRPVar( "money" ) end,
+		[ "pointshop1" ] = function( player ) amount = player:PS_GetPoints() end,
+		[ "pointshop2_standard" ] = function( player ) if player.PS2_Wallet then amount = player.PS2_Wallet.points end end,
+		[ "pointshop2_premium" ] = function( player ) if player.PS2_Wallet then amount = player.PS2_Wallet.premiumPoints end end
+	}
   
-  if amount == nil or !currencyTypes[ type:lower() ] then
-    error "Invalid currency set!"
-  end 
+	if amount == nil or !currencyTypes[ type:lower() ] then
+		error "Invalid currency set!"
+	end 
   
-  currencyTypes[ type ]( player )
+	currencyTypes[ type ]( player )
 end
